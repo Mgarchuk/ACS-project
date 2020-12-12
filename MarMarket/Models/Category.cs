2fe08a19cc500f8e7483d78052e197568621e3bd
Marina <mgarchuk6@gmail.com>
2020-12-12 13:42:24 +0300

    Add primitive models

---
+++ MarMarket/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarMarket.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public List<Product> Products { get; set; }
+
+    }
+}
---
+++ MarMarket/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace MarMarket.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Text { get; set; }
+
+        public virtual User Author { get; set; }
+
+
+    }
+}
